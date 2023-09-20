ed3c7332340a3d2ccd3040587accb07bf425492d
Marta Urbano de Felipe <martaurbanodefelipe@MacBook-Air-de-Marta.local>
2023-09-20 21:50:21 +0200

    all

--- Makefile
+++ Makefile
@@ -6,18 +6,19 @@
 #    By: jaurbano <jaurbano@student.42madrid.com    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/09/15 11:52:06 by jaurbano          #+#    #+#              #
-#    Updated: 2023/09/20 16:59:58 by jaurbano         ###   ########.fr        #
+#    Updated: 2023/09/20 17:14:01 by jaurbano         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
  
-SRCS	=	ft_isalpha.c
+SRCS	=		ft_isalpha.c
 
 OBJS	=	$(SRCS:.c=.o)
 
 CC		=	gcc
 
 RM		=	rm -f
+
 CFLAGS	=	-Wall -Wextra -Werror -I.
 
 INCLUDE	= 	libft.h
--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaurbano <jaurbano@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jaurbano <jaurbano@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 12:43:04 by jaurbano          #+#    #+#             */
-/*   Updated: 2023/09/20 14:09:20 by jaurbano         ###   ########.fr       */
+/*   Updated: 2023/09/20 21:48:57 by jaurbano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,52 +16,5 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-int			ft_isalpha(int a);
-
-int			ft_isdigit(int a);
-
-int			ft_isalnum(int a);
-
-int			ft_isascii(int c);
-
-int			ft_isprint(int a);
-
-size_t		ft_strlen(const char *str);
-
-void		*ft_memset(void *str, int c, size_t len);
-
-void		ft_bzero(void *s, size_t n);
-
-void		*ft_memcpy(void *dst, const void *src, size_t n);
-
-void		*ft_memmove(void *dst, const void *src, size_t len);
-
-size_t		ft_strlcpy(char *dest, char *src, unsigned int size);
-
-size_t		ft_strlcat(char *dest, char *src, size_t size);
-
-int			ft_toupper(int c);
-
-int			ft_tolower(int c);
-
-char		*ft_strchr(const char	*s, int c);
-
-char		*ft_strrchr(const char *s, int c);
-
-int			ft_strncmp(const char *s1, const char *s2, size_t n);
-
-void		*ft_memchr(void *s, int c, size_t n);
-
-int			ft_memcmp(void *s1, void *s2, size_t n);
-
-char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
-
-int			ft_atoi(const char *str);
-
-void		*ft_calloc(size_t count, size_t size);
-
-char		*ft_strdup(char *src);
-
-char		*ft_substr(char *s, unsigned int start, size_t len);
-
+int			ft_isalpha(int c);
 #endif
